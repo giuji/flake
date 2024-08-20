5bfec2e62d3b34b8d6b2793cc051df1704d77c0b
giuji <64275280+giuji@users.noreply.github.com>
2024-08-21 01:45:21 +0200

    Disable syncthing global discovery and relays

--- profiles/syncthing.nix
+++ profiles/syncthing.nix
@@ -6,9 +6,17 @@
     enable = true;
     relay.enable = false;
     openDefaultPorts = true;
+    extraFlags = [ "--no-default-folder" ];
     user = "giuji";
     dataDir = "/home/giuji/Sync";
     configDir = "/home/giuji/.config/syncthing";
+    settings = {
+      options = {
+        relaysEnabled = false;
+        globalAnnounceEnabled = false;
+        natEnabled = false;
+      };
+    };
   };
   
 }
