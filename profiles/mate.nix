f0314b2c808c1a253fe04039e3d5f954efa096cc
giuji <64275280+giuji@users.noreply.github.com>
2024-08-20 19:50:46 +0200

    Added disabled picom config

--- profiles/mate.nix
+++ profiles/mate.nix
@@ -28,6 +28,10 @@
     pulse.enable = true;
   };
 
+  # environment.systemPackages = with pkgs; [
+  #   picom
+  # ];
+
   home-manager.users.giuji = {
     gtk = {
       enable = true;
@@ -53,10 +57,35 @@
       style.name = "gtk2";
       platformTheme.name = "gtk2";
     };
-    home.file.".xinitrc" = {
-      enable = true;
-      text = "exec mate-session";
-    };
+  };
+
+  # home-manager.users.giuji.services.picom =
+  #   let
+  #     excluded = [
+  #       "window_type *= 'menu'"
+  #       "window_type *= 'dropdown_menu'"
+  #       "window_type *= 'popup_menu'"
+  #       "window_type *= 'utility'"
+  #       "window_type *= 'tooltip'"
+  #     ];
+  #   in {
+  #     enable = true;
+  #     backend = "egl";
+  #     fade = true;
+  #     fadeDelta = 5;
+  #     fadeExclude = excluded;
+  #     shadow = true;
+  #     #shadowOffsets = [ -18 -18 ];
+  #     shadowOpacity = 0.65;
+  #     shadowExclude = excluded;
+  #     vSync = true;
+  # };
+  
+  home-manager.users.giuji.home.file.".xinitrc" = {
+    enable = true;
+    text = ''
+     exec mate-session
+    '';
   };
 
 }
