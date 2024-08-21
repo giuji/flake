57e03713b0b73de1630f15bd35f17333405dbbf1
giuji <64275280+giuji@users.noreply.github.com>
2024-08-21 21:20:16 +0200

    switched to stable

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "help me i need help";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware";
