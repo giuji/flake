48cb55fad58819ba3c658e8fda09abecf2a95930
giuji <64275280+giuji@users.noreply.github.com>
2024-08-22 16:34:22 +0200

    Added tlp config

--- hosts/commonConfig.nix
+++ hosts/commonConfig.nix
@@ -24,5 +24,12 @@
     nheko
     keepassxc
   ];
+
+  services.tlp.settings = lib.mkIf config.services.tlp.enable {
+    CPU_BOOST_ON_AC = 1;
+    CPU_BOOST_ON_BAT = 0;
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  };
   
 }
