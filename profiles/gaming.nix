d7b99b2901487ee5c6b189b830d8e8e30a5a58a6
giuji <64275280+giuji@users.noreply.github.com>
2024-12-03 21:38:40 +0100

    Added gamescope and mangohud config

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -1,22 +1,33 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-
-  # hardware.opengl = {
-  #   enable = true;
-  #   driSupport = true;
-  #   driSupport32Bit = true;
-  # };
 
   programs.steam = {
     enable = true;
     gamescopeSession.enable = false;
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+    remotePlay.openFirewall = true;
+    fontPackages = (builtins.filter lib.types.package.check config.fonts.packages) ++ (with pkgs; [
+      liberation_ttf
+      wqy_zenhei
+    ]);
+    #extest.enable = true;
   };
 
-  hardware.steam-hardware.enable = true;
+  # also includes mangohud config
+  programs.gamescope = {
+    enable = true;
+    args = [ "--rt" "--mangoapp" ] ++ ["-W 1920" "-H 1080" "-f"];
+    env = {
+      MANGOHUD_CONFIG = "fsr,gamemode";
+    };
+  };
+
+  programs.gamemode.enable = false;
 
   environment.systemPackages = with pkgs; [
     ppsspp
+    mangohud
   ];
 
 }
